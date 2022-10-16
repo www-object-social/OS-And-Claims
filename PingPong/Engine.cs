ae70e8fc8579b429e8a856035aaa672da8a334b6
Pirasath <connect@object.social>
2022-10-16 21:35:21 +0200

    Update PingPong.Engine

--- PingPong/Engine.cs
+++ PingPong/Engine.cs
@@ -1,4 +1,6 @@
-﻿namespace PingPong;
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace PingPong;
 public class Engine
 {
     private readonly Product.Infomation PI;
@@ -10,13 +12,14 @@ public class Engine
         (this.UI = UI).Change += UI_Change;
         this.UI_Change();
     }
-
-    private void UI_Change()
+    private HubConnection Hub { get; set; } = null!;
+    private async void UI_Change()
     {
-        Console.WriteLine("UI_Change");
+        if (Hub != null ||this.PmT.Status  is Progress.manager.Status.Install or Progress.manager.Status.Cancel || Hub.State is not HubConnectionState.Disconnected) return;
         if (this.UI.Network is Unit.infomation.Network.Online)
         {
-
+            this.Hub = new HubConnectionBuilder().WithUrl("https://win-9ndprc00ff9.object.social/PongPing.Services").WithAutomaticReconnect().Build();
+            await this.Hub.StartAsync();
         }
         else this.PmT.Cancel(); 
     }
