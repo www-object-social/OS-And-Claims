e17b665061cab4e00685da682c08c30ed7c90db0
Pirasath <connect@object.social>
2022-10-23 20:17:30 +0200

    Update whit prefix

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -1,7 +1,21 @@
 ﻿using PhoneNumbers;
 
 var a = PhoneNumbers.CountryCodeToRegionCodeMap.GetCountryCodeToRegionCodeMap();
-var b = PhoneNumberUtil.GetInstance();
-var c = b.Parse("50139000993", "DK");
-var d = b.IsValidNumber(c);
-Console.WriteLine(d);
+//var b = PhoneNumberUtil.GetInstance();
+//var c = b.Parse("50139000993", "DK");
+//var d = b.IsValidNumber(c);
+//Console.WriteLine(d);
+var d = PhoneNumberUtil.GetInstance();
+foreach (var b in a.Where(x=>x.Value.First()!="World"))
+{
+	try
+	{
+		var g = new System.Globalization.RegionInfo(b.Value.First());
+		
+		Console.WriteLine($"                <div class=\"item\">\r\n                    <div class=\"iso6311\">{g.TwoLetterISORegionName}</div>\r\n                    <div class=\"prefix-number\">+{b.Key}</div>\r\n                    <div class=\"country\">{g.EnglishName}</div>\r\n                </div>");
+	}
+	catch (Exception)
+	{
+
+	}
+}
